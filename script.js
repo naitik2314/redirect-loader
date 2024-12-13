59b9dd77be71b5e0b82e864a305d42b17a7fcbaf
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2024-12-13 15:15:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//Inspired from some random image online
+const dots = document.getElementById("dots");
+let dotCount = 0;
+
+setInterval(() => {
+  dotCount = (dotCount + 1) % 4; // Cycle between 0 to 3 dots
+  dots.textContent = ".".repeat(dotCount);
+}, 500);
