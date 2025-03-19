7dd13093d60ac519af216aec0d2ac14f94b90a35
Naitik Shah <63101485+naitik2314@users.noreply.github.com>
2025-03-18 22:40:06 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const dots = document.getElementById("dots");
-let dotCount = 0;
-
-setInterval(() => {
-  dotCount = (dotCount + 1) % 4; // Cycle between 0 to 3 dots
-  dots.textContent = ".".repeat(dotCount);
-}, 500);
